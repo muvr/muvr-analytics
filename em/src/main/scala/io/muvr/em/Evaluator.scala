1fae107ce86f66751ddcb6f41683232b7c0d1d5c
Jan Machacek <jan.machacek@gmail.com>
2015-11-21 13:57:43 +0000

    Evaluator

--- em/src/main/scala/io/muvr/em/Evaluator.scala
+++ em/src/main/scala/io/muvr/em/Evaluator.scala
@@ -7,15 +7,18 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 
 object Evaluator extends App {
   val dataset = new CuratedExerciseDataSetLoader(
-    trainDirectory = new File("/Users/janmachacek/Tmp/labelled/core"),
+    trainDirectory = new File("/Users/janmachacek/Muvr/muvr-open-training-data/test/core"),
     multiplier = 10)
 
+  println("Loading dataset...")
   val (examples, labels) = dataset.train
 
+  println("Loading model...")
   val ois = new ObjectInputStream(new FileInputStream("/Users/janmachacek/Tmp/model.ser"))
   val model = ois.readObject().asInstanceOf[MultiLayerNetwork]
   ois.close()
 
+  println("Evaluating...")
   val (s, f) = Evaluation.evaluate(model, examples, labels)
   println(s"Succeeded $s, failed $f")
 }
