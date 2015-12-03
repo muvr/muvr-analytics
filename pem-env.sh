bd983c3c3f6c541dbb854d1966cefc605a153e48
Jan Machacek <jan.machacek@gmail.com>
2015-12-03 18:52:29 +0000

    Latest

--- pem-env.sh
+++ pem-env.sh
@@ -23,27 +23,14 @@ cd $VENV/neon
 make sysinstall
 cd -
 
-# Install mlp
-cd mlp
+# Install pem
+cd pem
 python setup.py install
 cd -
 
-# Install Spark
-cd $VENV
-
-if ! [ `command -v pv` ]
-then
-  brew install pv
-fi
-curl http://mirror.catn.com/pub/apache/spark/spark-1.5.1/spark-1.5.1-bin-hadoop2.6.tgz -o spark-1.5.1-bin-hadoop2.6.tgz
-[ $(pv spark-1.5.1-bin-hadoop2.6.tgz | tar xzf -) ] || tar xzf spark-1.5.1-bin-hadoop2.6.tgz
-
-cd -
-SPARK_HOME=`pwd`/$VENV/spark-1.5.1-bin-hadoop2.6
 cat >> $VENV/bin/activate << EOF
 export SPARK_HOME=$SPARK_HOME
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/build:$PYTHONPATH
 export PATH=$SPARK_HOME/bin:$PATH
 EOF
 source $VENV/bin/activate
-
