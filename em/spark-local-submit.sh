dc7694dd60eaa6cf238c05f41b6cb4a0c16942b3
Jan Machacek <jan.machacek@gmail.com>
2015-12-04 18:49:30 +0000

    Working

--- em/spark-local-submit.sh
+++ em/spark-local-submit.sh
@@ -2,4 +2,4 @@
 
 TRAINING_DATA_DIR="/Users/janmachacek/Muvr/muvr-open-training-data"
 PARAMS="--master local --model arms --train-path $TRAINING_DATA_DIR/train --test-path $TRAINING_DATA_DIR/test --output-path s3n://AKIAIWGORVA3QVK54PMA:emJ7hW7dup1Jg5aCcYcevFE5AGXLtH5zrV0Ko3W+@muvr-open-training-data/models $@"
-spark-submit --class io.muvr.em.ModelTrainerMain --driver-memory 4G --driver-class-path target/scala-2.10/em-assembly-1.0.0-SNAPSHOT.jar target/scala-2.10/em-assembly-1.0.0-SNAPSHOT.jar $PARAMS
+spark-submit --class io.muvr.em.ModelTrainerMain --driver-memory 4G target/scala-2.10/em-assembly-1.0.0-SNAPSHOT.jar $PARAMS
